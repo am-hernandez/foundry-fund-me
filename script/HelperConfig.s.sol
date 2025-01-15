4af46b5c944b1d15a32c535afc6963e92adc14ac
Angel Hernandez <angel@magic.link>
2025-01-15 00:30:29 -0500

    update: formatting

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -35,25 +35,19 @@ contract HelperConfig is Script {
     }
 
     function getEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory ethConfig = NetworkConfig({
-            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
-        });
+        NetworkConfig memory ethConfig = NetworkConfig({priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419});
 
         return ethConfig;
     }
 
     function getBaseConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory baseConfig = NetworkConfig({
-            priceFeed: 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70
-        });
+        NetworkConfig memory baseConfig = NetworkConfig({priceFeed: 0x71041dddad3595F9CEd3DcCFBe3D1F4b0a16Bb70});
 
         return baseConfig;
     }
 
     function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
-        NetworkConfig memory sepoliaEthConfig = NetworkConfig({
-            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
-        });
+        NetworkConfig memory sepoliaEthConfig = NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
 
         return sepoliaEthConfig;
     }
@@ -67,15 +61,10 @@ contract HelperConfig is Script {
         // 2. return the mock address
 
         vm.startBroadcast();
-        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
-            DECIMALS,
-            INITIAL_PRICE
-        );
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
         vm.stopBroadcast();
 
-        NetworkConfig memory anvilEthConfig = NetworkConfig({
-            priceFeed: address(mockPriceFeed)
-        });
+        NetworkConfig memory anvilEthConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
 
         return anvilEthConfig;
     }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -83,10 +83,7 @@ contract FundMeTest is Test {
         uint256 endingOwnerBalance = fundMe.getOwner().balance;
         uint256 endingFundMeBalance = address(fundMe).balance;
         assertEq(endingFundMeBalance, 0);
-        assertEq(
-            startingOwnerBalance + startingFundMeBalance,
-            endingOwnerBalance
-        );
+        assertEq(startingOwnerBalance + startingFundMeBalance, endingOwnerBalance);
     }
 
     function testWithdrawWithMultipleFunders() public funded {
@@ -110,9 +107,6 @@ contract FundMeTest is Test {
 
         // Assert
         assert(address(fundMe).balance == 0);
-        assert(
-            startingFundMeBalance + startingOwnerBalance ==
-                fundMe.getOwner().balance
-        );
+        assert(startingFundMeBalance + startingOwnerBalance == fundMe.getOwner().balance);
     }
 }
